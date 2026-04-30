1bd204177cf9e564e05f4058af3b3d78fcd87244
APIBox <LinkerVB@outlook.com>
2026-04-30 23:40:31 +0800

    feat: Vm 支持了加减乘除运算

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,13 +1,19 @@
 pub mod err;
 pub mod test;
 
+const NO_OBJECT_HERE: &str = "no object here";
+
 pub type VmResult<T> = Result<T, String>;
 
 pub const STACK_SIZE: usize = 2048;
 pub const GLOBALS_SIZE: usize = 65535;
 
 use crate::{
-    object::{Object, func::Func},
+    object::{
+        Object,
+        func::Func,
+        object_operators::{ObjAdd, ObjDiv, ObjMul, ObjSub},
+    },
     vm::opcode::{Op, OpKind},
 };
 
@@ -54,6 +60,43 @@ impl Vm {
 }
 
 impl Vm {
+    fn obj_operator(&mut self, op: OpKind) -> VmResult<()> {
+        if ![OpKind::Add, OpKind::Div, OpKind::Sub, OpKind::Mul].contains(&op) {
+            return Ok(());
+        }
+
+        let right_obj = self
+            .pop()
+            .map_or_else(|| VmResult::Err(NO_OBJECT_HERE.to_owned()), |it| Ok(it))?;
+
+        let left_obj = self
+            .pop()
+            .map_or_else(|| VmResult::Err(NO_OBJECT_HERE.to_owned()), |it| Ok(it))?;
+
+        let op_functions = [
+            Object::obj_add,
+            Object::obj_sub,
+            Object::obj_mul,
+            Object::obj_div,
+        ];
+
+        let op_func = op_functions[op as usize - 1];
+        let o = op_func(&left_obj, &right_obj).map_or_else(
+            || {
+                VmResult::Err(format!(
+                    "invaild operator to {} and {}",
+                    left_obj.type_name(),
+                    right_obj.type_name(),
+                ))
+            },
+            |it| Ok(it),
+        )?;
+
+        self.push(o)?;
+
+        Ok(())
+    }
+
     pub fn run(&mut self) -> VmResult<()> {
         while if self.current_frame_ref().ip >= 0 {
             self.current_frame_ref().ip as usize
@@ -70,6 +113,11 @@ impl Vm {
                 OpKind::Const => {
                     self.push(self.constants[op.operands[0] as usize].clone())?;
                 }
+
+                OpKind::Add => self.obj_operator(op.kind)?,
+                OpKind::Sub => self.obj_operator(op.kind)?,
+                OpKind::Mul => self.obj_operator(op.kind)?,
+                OpKind::Div => self.obj_operator(op.kind)?,
 
                 OpKind::Pop => {
                     if self.sp != 0 {
