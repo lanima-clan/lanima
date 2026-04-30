d9a9cbcd9f1bd6c98da099c8adc525fd86588fe1
APIBox <LinkerVB@outlook.com>
2026-04-30 23:41:42 +0800

    test: 新增加减乘除测试

--- src/vm/test.rs
+++ src/vm/test.rs
@@ -1,10 +1,13 @@
 #[cfg(test)]
 mod tests {
+    use bigdecimal::BigDecimal;
+
     use crate::{
+        gc,
         object::Object,
         vm::{
             Vm,
-            opcode::{OpKind, make},
+            opcode::{Op, OpKind, make},
         },
     };
 
@@ -20,9 +23,65 @@ mod tests {
         vm.run().unwrap();
 
         assert!(vm.last_popped().is_some());
-        assert_eq!(
-            vm.last_popped().unwrap(),
-            expected_obj
-        )
+        assert_eq!(vm.last_popped().unwrap(), expected_obj)
+    }
+
+    #[test]
+    fn test_add_sub_mul_div() {
+        let constants = vec![
+            Object::I64(10),
+            Object::I64(5),
+        ];
+
+        let test_cases = vec![
+            (
+                vec![
+                    make(OpKind::Const, &[0]),
+                    make(OpKind::Const, &[1]),
+                    make(OpKind::Add, &[]),
+                    make(OpKind::Pop, &[]),
+                ],
+                Object::I64(15),
+            ),
+            (
+                vec![
+                    make(OpKind::Const, &[0]),
+                    make(OpKind::Const, &[1]),
+                    make(OpKind::Sub, &[]),
+                    make(OpKind::Pop, &[]),
+                ],
+                Object::I64(5),
+            ),
+            (
+                vec![
+                    make(OpKind::Const, &[0]),
+                    make(OpKind::Const, &[1]),
+                    make(OpKind::Mul, &[]),
+                    make(OpKind::Pop, &[]),
+                ],
+                Object::I64(50),
+            ),
+            (
+                vec![
+                    make(OpKind::Const, &[0]),
+                    make(OpKind::Const, &[1]),
+                    make(OpKind::Div, &[]),
+                    make(OpKind::Pop, &[]),
+                ],
+                Object::Decimal(gc!(BigDecimal::from(2))),
+            ),
+        ];
+
+        for (ins, expected) in test_cases {
+            test_expected(&ins, constants.clone(), expected);
+        }
+    }
+
+    fn test_expected(instructions: &[Op], constants: Vec<Object>, expected: Object) {
+        let mut vm = Vm::new(&instructions, constants);
+        vm.run().unwrap();
+
+        assert!(vm.last_popped().is_some());
+        assert_eq!(vm.last_popped().unwrap(), expected);
     }
 }
