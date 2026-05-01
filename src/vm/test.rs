bd18b379361a1855ce458c5e9a0924d8edcbe1c1
APIBox <LinkerVB@outlook.com>
2026-05-01 14:08:43 +0800

    feat: 新增函数测试

--- src/vm/test.rs
+++ src/vm/test.rs
@@ -1,10 +1,12 @@
 #[cfg(test)]
 mod tests {
+    use std::time::Instant;
+
     use bigdecimal::BigDecimal;
 
     use crate::{
         gc,
-        object::Object,
+        object::{Object, func::Func},
         vm::{
             Vm,
             opcode::{Op, OpKind, make},
@@ -27,11 +29,75 @@ mod tests {
     }
 
     #[test]
-    fn test_add_sub_mul_div() {
-        let constants = vec![
-            Object::I64(10),
-            Object::I64(5),
+    fn test_simplest_function() {
+        let instructions = vec![
+            make(OpKind::Const, &[0]),
+            make(OpKind::Call, &[0]),
+            make(OpKind::Pop, &[]),
         ];
+
+        let func_instructions = vec![make(OpKind::Return, &[])];
+
+        let constants = vec![Object::Func(gc!(Func {
+            instructions: func_instructions.into(),
+            constants: vec![]
+        }))];
+
+        test_expected(&instructions, constants, Object::Null);
+    }
+
+    #[test]
+    fn test_fib_function() {
+        let instructions = vec![
+            make(OpKind::Const, &[0]), // func fib
+            make(OpKind::Const, &[1]), // n
+            make(OpKind::Call, &[1]),
+            make(OpKind::Pop, &[]),
+        ];
+
+        let func_instructions = vec![
+            // 3 > n
+            make(OpKind::Const, &[0]),
+            make(OpKind::GetLocal, &[0]),
+            make(OpKind::Gt, &[]),
+            make(OpKind::JumpNotTruthy, &[7]),
+
+            // 3 > n 的情况
+            make(OpKind::Const, &[1]), // 3 > n: 1
+            make(OpKind::ReturnValue, &[]),
+
+            // fib(n - 1) + fib(n - 2)
+            make(OpKind::CurrentFunc, &[]),
+            make(OpKind::GetLocal, &[1]),
+            make(OpKind::Const, &[1]),
+            make(OpKind::Sub, &[]),
+            make(OpKind::Call, &[1]),
+            make(OpKind::CurrentFunc, &[]),
+            make(OpKind::GetLocal, &[0]),
+            make(OpKind::Const, &[2]),
+            make(OpKind::Sub, &[]),
+            make(OpKind::Call, &[1]),
+            make(OpKind::Add, &[]),
+            make(OpKind::ReturnValue, &[]),
+        ];
+
+        let constants = vec![
+            Object::Func(gc!(Func {
+                instructions: func_instructions.into(),
+                constants: vec![Object::I64(3), Object::I64(1), Object::I64(2),],
+            })),
+            Object::I64(35),
+            Object::I64(3),
+        ];
+
+        let timer = Instant::now();
+        test_expected(&instructions, constants, Object::I64(9227465));
+        println!("{:#?}", timer.elapsed());
+    }
+
+    #[test]
+    fn test_operators() {
+        let constants = vec![Object::I64(10), Object::I64(5)];
 
         let test_cases = vec![
             (
