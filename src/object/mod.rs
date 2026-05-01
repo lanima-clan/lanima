dc2d6d0f0ca1d67af30e126ceda240c29cc95d26
APIBox <LinkerVB@outlook.com>
2026-05-01 14:07:40 +0800

    feat: 将 Func 纳入 Gc 对象, 现在对象可判断是否为真 新增常量对象真假 新增布尔对象

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,5 +1,5 @@
-pub mod object_operators;
 pub mod func;
+pub mod object_operators;
 
 pub type GcObject = Rc<RefCell<Object>>;
 pub type Gc<T> = Rc<RefCell<T>>;
@@ -7,16 +7,18 @@ pub type Gc<T> = Rc<RefCell<T>>;
 use std::{cell::RefCell, rc::Rc};
 
 use bigdecimal::BigDecimal;
+use num_traits::Zero;
 
 use crate::object::func::Func;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Object {
     I64(i64),
+    Bool(bool),
     Decimal(Gc<BigDecimal>),
-    Func(Func),
+    Func(Gc<Func>),
     Vec(Gc<Vec<Object>>),
-    Null
+    Null,
 }
 
 impl Object {
@@ -26,7 +28,23 @@ impl Object {
             Object::Decimal(_) => "float".to_owned(),
             Object::Func(_) => "Function".to_owned(),
             Object::Vec(_) => "Vec".to_owned(),
+            Object::Bool(_) => "bool".to_owned(),
             Object::Null => "null".to_owned(),
+        }
+    }
+}
+
+impl Object {
+    pub fn is_truthy(&self) -> bool {
+        match self {
+            Object::Bool(it) => *it,
+            Object::I64(it) => *it != 0,
+            Object::Decimal(it) => !it.borrow().is_zero(),
+            Object::Func(func) => {
+                !func.borrow().instructions.is_empty() && !func.borrow().constants.is_empty()
+            }
+            Object::Vec(v) => !v.borrow().is_empty(),
+            Object::Null => false,
         }
     }
 }
@@ -36,4 +54,15 @@ macro_rules! gc {
     ($o:expr) => {
         std::rc::Rc::new(std::cell::RefCell::new($o))
     };
+}
+
+pub const TRUE_OBJ: Object = Object::Bool(true);
+pub const FALSE_OBJ: Object = Object::Bool(false);
+
+pub fn native_bool_to_obj(native_bool: bool) -> Object {
+    if native_bool {
+        TRUE_OBJ
+    } else {
+        FALSE_OBJ
+    }
 }
