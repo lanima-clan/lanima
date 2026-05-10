78fdc4eddc43852b5e80e6596c015d47e2402c99
APIBox <LinkerVB@outlook.com>
2026-05-10 08:26:14 +0800

    feat: 添加内置函数类型

--- src/object/func.rs
+++ src/object/func.rs
@@ -1,9 +1,26 @@
 use std::rc::Rc;
 
-use crate::{object::Object, vm::opcode::Op};
+use crate::{
+    object::Object,
+    vm::{Vm, opcode::Op},
+};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Func {
     pub instructions: Rc<[Op]>,
-    pub constants: Vec<Object>
+    pub constants: Vec<Object>,
 }
+
+pub type NativeFuncResult = crate::vm::VmResult<Option<crate::object::Object>>;
+
+#[derive(Debug, Clone)]
+pub struct NativeFunc(pub fn(&mut Vm, Vec<Object>) -> NativeFuncResult);
+
+impl PartialEq for NativeFunc {
+    fn eq(&self, other: &Self) -> bool {
+        // 使用 ptr::fn_addr_eq 进行可靠的函数指针比较
+        std::ptr::fn_addr_eq(self.0, other.0)
+    }
+}
+
+impl Eq for NativeFunc {}
