43b53f53829b99ee4c648aa588a294d270414b34
APIBox <LinkerVB@outlook.com>
2026-05-10 08:26:32 +0800

    feat: 添加内置函数类型, 为 Object 实现 Display

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -4,12 +4,12 @@ pub mod object_operators;
 pub type GcObject = Rc<RefCell<Object>>;
 pub type Gc<T> = Rc<RefCell<T>>;
 
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use bigdecimal::BigDecimal;
 use num_traits::Zero;
 
-use crate::object::func::Func;
+use crate::object::func::{Func, NativeFunc};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Object {
@@ -17,6 +17,7 @@ pub enum Object {
     Bool(bool),
     Decimal(Gc<BigDecimal>),
     Func(Gc<Func>),
+    NativeFunc(NativeFunc),
     Vec(Gc<Vec<Object>>),
     Null,
 }
@@ -27,6 +28,7 @@ impl Object {
             Object::I64(_) => "i64".to_owned(),
             Object::Decimal(_) => "float".to_owned(),
             Object::Func(_) => "Function".to_owned(),
+            Object::NativeFunc(_) => "NativeFunction".to_owned(),
             Object::Vec(_) => "Vec".to_owned(),
             Object::Bool(_) => "bool".to_owned(),
             Object::Null => "null".to_owned(),
@@ -45,6 +47,34 @@ impl Object {
             }
             Object::Vec(v) => !v.borrow().is_empty(),
             Object::Null => false,
+            Object::NativeFunc(_it) => true,
+        }
+    }
+}
+
+impl Display for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Object::Bool(it) => write!(f, "{it}"),
+            Object::I64(it) => write!(f, "{it}"),
+            Object::Decimal(it) => write!(f, "{}", it.borrow()),
+            Object::Func(func) => write!(
+                f,
+                "<function instructions: {:?} constants: {:?}>",
+                func.borrow().instructions,
+                func.borrow().constants
+            ),
+            Object::Vec(v) => write!(
+                f,
+                "[{}]",
+                v.borrow()
+                    .iter()
+                    .map(|it| it.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            ),
+            Object::Null => write!(f, "null"),
+            Object::NativeFunc(it) => write!(f, "<nativefunction ptr: {:?}>", it.0),
         }
     }
 }
@@ -60,9 +90,8 @@ pub const TRUE_OBJ: Object = Object::Bool(true);
 pub const FALSE_OBJ: Object = Object::Bool(false);
 
 pub fn native_bool_to_obj(native_bool: bool) -> Object {
-    if native_bool {
-        TRUE_OBJ
-    } else {
-        FALSE_OBJ
-    }
+    if native_bool { TRUE_OBJ } else { FALSE_OBJ }
 }
+
+unsafe impl Send for Object {}
+unsafe impl Sync for Object {}
